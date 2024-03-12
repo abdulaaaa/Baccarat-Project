603589bbb73efb85ce3234691b555c5801390a9d
abdulaaaa <awarikattayil@gmail.com>
2024-03-12 13:07:25 -0400

    added cardChosen() and also initializePlayerAndBanker()

--- src/main/java/org/project/baccarat/Card.java
+++ src/main/java/org/project/baccarat/Card.java
@@ -12,6 +12,14 @@
 package org.project.baccarat;
 
 class Card {
+    public Rank getRank() {
+        return rank;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
     private final Rank rank;
     private final Suit suit;
 
@@ -29,17 +37,27 @@ enum Suit {
 }
 
 enum Rank {
-    ACE,
-    TWO,
-    THREE,
-    FOUR,
-    FIVE,
-    SIX,
-    SEVEN,
-    EIGHT,
-    NINE,
-    TEN,
-    JACK,
-    QUEEN,
-    KING
+    ACE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    JACK(11),
+    QUEEN(12),
+    KING(13);
+
+    private final int value;
+
+    Rank(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
--- src/main/java/org/project/baccarat/Game.java
+++ src/main/java/org/project/baccarat/Game.java
@@ -1,4 +1,93 @@
 package org.project.baccarat;
 
+import java.util.List;
+
 public class Game {
+    private int playerCards;
+    private int bankerCards;
+    private float ratioForBankerPlayerTie;
+    private int placedBetAmount;
+
+    public int getPlayerCards() {
+        return playerCards;
+    }
+
+    public void setPlayerCards(int playerCards) {
+        this.playerCards = playerCards;
+    }
+
+    public int getBankerCards() {
+        return bankerCards;
+    }
+
+    public void setBankerCards(int bankerCards) {
+        this.bankerCards = bankerCards;
+    }
+
+    public float getRatioForBankerPlayerTie() {
+        return ratioForBankerPlayerTie;
+    }
+
+    public void setRatioForBankerPlayerTie(int chosenOption) {
+        switch (chosenOption) {
+            case 1 -> ratioForBankerPlayerTie = 1;
+            case 2 -> ratioForBankerPlayerTie = 8;
+            case 3 -> ratioForBankerPlayerTie = (float) 0.95;
+            default ->
+                    throw new IllegalArgumentException("Please choose a valid " +
+                            "option.");
+        }
+    }
+
+    public int getPlacedBetAmount() {
+        return placedBetAmount;
+    }
+
+    public void setPlacedBetAmount(int placedBetAmount) {
+        this.placedBetAmount = placedBetAmount;
+    }
+
+    public Game(int playerCards, int bankerCards, float ratioForBankerPlayerTie,
+                int placedBetAmount) {
+        this.playerCards = playerCards;
+        this.bankerCards = bankerCards;
+        this.ratioForBankerPlayerTie = ratioForBankerPlayerTie;
+        this.placedBetAmount = placedBetAmount;
+    }
+
+    DeckOfCards deck = new DeckOfCards();
+    List<Card> sevenDeckOfCards = deck.getSevenDeckOfCards();
+    List<Card> usedDeckOfCards = deck.getUsedCards();
+
+
+    public int cardsChosen() {
+        int cardValue = 0;
+        for (int i = 0; i < 2; i++) {
+            if ((sevenDeckOfCards.get(0).getRank().getValue() >= 2) &&
+                    (sevenDeckOfCards.get(0).getRank().getValue() <= 9)) {
+                cardValue += sevenDeckOfCards.get(0).getRank().getValue();
+                usedDeckOfCards.add(sevenDeckOfCards.get(0));
+                sevenDeckOfCards.remove(0);
+            } else if (sevenDeckOfCards.get(0).getRank().getValue() == 1) {
+                cardValue++;
+                usedDeckOfCards.add(sevenDeckOfCards.get(0));
+                sevenDeckOfCards.remove(0);
+            } else {
+                usedDeckOfCards.add(sevenDeckOfCards.get(0));
+                sevenDeckOfCards.remove(0);
+            }
+        }
+
+        return cardValue % 10;
+    }
+
+    public void initializePlayerAndBanker() {
+        playerCards = cardsChosen();
+        bankerCards = cardsChosen();
+    }
+
+
+
+    ;
+
 }
